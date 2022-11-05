6a53f7ad4068704871d24b27060220f8cd2fc44d
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-11-05 23:43:10 +0900

    Member 엔티티 필드 및 생성자 추가

--- my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/domain/Member.java
+++ my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/domain/Member.java
@@ -3,16 +3,16 @@ package kr.ac.kumoh.oiyo.mydiaryback.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 
 @Entity
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-public class Member extends BaseEntity{
+public class Member extends BaseEntity {
 
     @Id
     @Column(name = "MEMBER_ID")
@@ -30,7 +30,21 @@ public class Member extends BaseEntity{
     // 사용자 생년월일
     private LocalDate birthDate;
 
+    // 사용자 이메일
+    private String email;
+
     // 사용자 주소
     @Embedded
     private Address address;
+
+    public Member(LocalDateTime createDate, LocalDateTime lastModifiedDate, String id, String name, String profileImg, String profileIntroduction, LocalDate birthDate, String email, Address address) {
+        super(createDate, lastModifiedDate);
+        this.id = id;
+        this.name = name;
+        this.profileImg = profileImg;
+        this.profileIntroduction = profileIntroduction;
+        this.birthDate = birthDate;
+        this.email = email;
+        this.address = address;
+    }
 }
