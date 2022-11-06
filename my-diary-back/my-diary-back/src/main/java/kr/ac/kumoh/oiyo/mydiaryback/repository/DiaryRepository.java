90ca86ae977429b50ef11fea318ce823b95519d5
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-11-06 23:30:10 +0900

    DiaryRepository 함수명 변경

--- my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/repository/DiaryRepository.java
+++ my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/repository/DiaryRepository.java
@@ -27,7 +27,7 @@ public class DiaryRepository {
     }
 
     /**
-     * find(diaryId) 함수로 하나의 Diary 객체 조회
+     * 하나의 Diary 객체 조회
      * 일기 목록에서 특정 일기 클릭하면 호출된다.
      * 일기 제목, 여행 날짜, 일기 본문, 날씨, 여행지, 일기 생성 시간, 일기 수정 시간
      *
@@ -47,13 +47,13 @@ public class DiaryRepository {
     /**
      * 여행별 일기 조회 메서드 (날짜 오름차순)
      * 지도에서 마커를 클릭하면 해당 메서드가 호출된다.
-     * 일기 리스트에는 제목, 여행 날짜, 날씨, 생성 날짜, 수정 날짜만 표시
+     * 일기 리스트에는 제목, 여행 날짜, 날씨만 표시
      * 일기 id도 함께 보내준다.
      *
      * @param travelId 클릭한 마커에 해당하는 여행Id
      * @return travelId에 해당하는 모든 일기 조회
      */
-    public List<FindDiaryDtoByTravel> findDiaryDtoByTravel(Long travelId) {
+    public List<FindDiaryDtoByTravel> findDiariesByTravel(Long travelId) {
         return em.createQuery("select new kr.ac.kumoh.oiyo.mydiaryback.repository.dto.FindDiaryDtoByTravel"
                                 + "(d.id, d.title, d.travelDate, d.weather) from Diary d "
                                 + "join d.travel t on t.id = :tId ORDER BY d.travelDate",
