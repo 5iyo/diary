23da825f1093a1328301d9be295cc2d661de1414
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-11-06 22:49:59 +0900

    DiaryRepository 메서드 추가

--- my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/repository/DiaryRepository.java
+++ my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/repository/DiaryRepository.java
@@ -1,6 +1,8 @@
 package kr.ac.kumoh.oiyo.mydiaryback.repository;
 
 import kr.ac.kumoh.oiyo.mydiaryback.domain.Diary;
+import kr.ac.kumoh.oiyo.mydiaryback.repository.dto.FindDiaryDtoByTravel;
+import kr.ac.kumoh.oiyo.mydiaryback.repository.dto.FindOneDiaryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -15,6 +17,7 @@ public class DiaryRepository {
 
     /**
      * 일기 저장
+     *
      * @param diary 엔티티 객체
      * @return diaryId (pk)
      */
@@ -25,30 +28,58 @@ public class DiaryRepository {
 
     /**
      * find(diaryId) 함수로 하나의 Diary 객체 조회
+     * 일기 목록에서 특정 일기 클릭하면 호출된다.
+     * 일기 제목, 여행 날짜, 일기 본문, 날씨, 여행지, 일기 생성 시간, 일기 수정 시간
+     *
      * @param diaryId - pk
      * @return 조회한 diary 엔티티 객체
      */
-    public Diary findOne(Long diaryId) {
-        return em.find(Diary.class, diaryId);
+    public FindOneDiaryDto findOne(Long diaryId) {
+        return em.createQuery("select new kr.ac.kumoh.oiyo.mydiaryback.repository.dto.FindOneDiaryDto"
+                        + "(d.title, d.travelDate, d.mainText, d.weather, t.travelDestination, d.createDate, d.lastModifiedDate) "
+                        + "from Diary d "
+                        + "join d.travel t where d.id = :dId"
+                        , FindOneDiaryDto.class)
+                .setParameter("dId", diaryId)
+                .getSingleResult();
     }
 
     /**
-     * 여행별 일기 조회 메서드
+     * 여행별 일기 조회 메서드 (날짜 오름차순)
      * 지도에서 마커를 클릭하면 해당 메서드가 호출된다.
+     * 일기 리스트에는 제목, 여행 날짜, 날씨, 생성 날짜, 수정 날짜만 표시
+     * 일기 id도 함께 보내준다.
+     *
      * @param travelId 클릭한 마커에 해당하는 여행Id
      * @return travelId에 해당하는 모든 일기 조회
      */
-    public List<Diary> findDiariesByTravel(Long travelId) {
-        return em.createQuery("select d from Diary d join d.travel t on t.id = :travelId", Diary.class)
-                .setParameter("travelId", travelId)
+    public List<FindDiaryDtoByTravel> findDiaryDtoByTravel(Long travelId) {
+        return em.createQuery("select new kr.ac.kumoh.oiyo.mydiaryback.repository.dto.FindDiaryDtoByTravel"
+                                + "(d.id, d.title, d.travelDate, d.weather) from Diary d "
+                                + "join d.travel t on t.id = :tId ORDER BY d.travelDate",
+                        FindDiaryDtoByTravel.class)
+                .setParameter("tId", travelId)
                 .getResultList();
     }
 
     /**
      * 사용자의 모든 일기 조회 (날짜 오름차순)
+     *
      * @return 사용자가 작성한 모든 일기
      */
-    public List<Diary> findAllByMember() {
+    /*public List<Diary> findAllByMember() {
         return em.createQuery("select d from Diary d ORDER BY d.createDate", Diary.class).getResultList();
-    }
+    }*/
+
+    /**
+     * 여행별 일기 조회 메서드 (날짜 오름차순)
+     *
+     * @param travelId 클릭한 마커에 해당하는 여행Id
+     * @return travelId에 해당하는 모든 일기 조회
+     */
+    /*public List<Diary> findDiariesByTravel(Long travelId) {
+        return em.createQuery("select d from Diary d join d.travel t on t.id = :travelId ORDER BY d.travelDate", Diary.class)
+                .setParameter("travelId", travelId)
+                .getResultList();
+    }*/
 }
--- my-diary-back/my-diary-back/src/test/java/kr/ac/kumoh/oiyo/mydiaryback/repository/DiaryRepositoryTest.java
+++ my-diary-back/my-diary-back/src/test/java/kr/ac/kumoh/oiyo/mydiaryback/repository/DiaryRepositoryTest.java
@@ -4,6 +4,8 @@ import kr.ac.kumoh.oiyo.mydiaryback.domain.Address;
 import kr.ac.kumoh.oiyo.mydiaryback.domain.Diary;
 import kr.ac.kumoh.oiyo.mydiaryback.domain.Member;
 import kr.ac.kumoh.oiyo.mydiaryback.domain.Travel;
+import kr.ac.kumoh.oiyo.mydiaryback.repository.dto.FindDiaryDtoByTravel;
+import kr.ac.kumoh.oiyo.mydiaryback.repository.dto.FindOneDiaryDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +30,47 @@ public class DiaryRepositoryTest {
     @Autowired
     private DiaryRepository diaryRepository;
 
+    private LocalDateTime now = LocalDateTime.of(2022, 11, 6, 22, 33, 16);
+
+    @Test
+    public void 일기조회_테스트() {
+
+        // Given
+        Member member = createMember("member1", "Member1", null, null, LocalDate.of(1999, 1, 1), "test@namer.com", null);
+        em.persist(member);
+
+        LocalDate sd = LocalDate.of(2022, 1, 1);
+        LocalDate ed = LocalDate.of(2022, 1, 3);
+        Travel travel = createTravel(member, "서울", "37.541°", "126.986", null, sd, ed);
+        em.persist(travel);
+
+        LocalDate diaryDate1 = LocalDate.of(2022, 1, 1);
+        LocalDate diaryDate2 = LocalDate.of(2022, 1, 2);
+        LocalDate diaryDate3 = LocalDate.of(2022, 1, 3);
+        Diary diary1 = createDiary("diary1", diaryDate1, "주절주절", "맑음", travel);
+        Diary diary2 = createDiary("diary2", diaryDate2, "절주절주", "흐림", travel);
+        Diary diary3 = createDiary("diary3", diaryDate3, "쥬절쥬절", "비", travel);
+        diaryRepository.save(diary1);
+        diaryRepository.save(diary2);
+        diaryRepository.save(diary3);
+
+        // When
+        em.flush();
+        em.clear();
+
+        FindOneDiaryDto f = diaryRepository.findOne(diary2.getId());
+
+        // Then
+        assertThat(f.getTitle()).isEqualTo("diary2");
+        assertThat(f.getTravelDate()).isEqualTo(diaryDate2);
+        assertThat(f.getMainText()).isEqualTo("절주절주");
+        assertThat(f.getWeather()).isEqualTo("흐림");
+        assertThat(f.getTravelDestination()).isEqualTo("서울");
+        assertThat(f.getCreatedDate()).isEqualTo(now);
+        assertThat(f.getLastModifiedDate()).isEqualTo(now);
+
+    }
+
     @Test
     public void 여행별_일기조회_테스트() {
 
@@ -46,31 +89,31 @@ public class DiaryRepositoryTest {
         Diary diary1 = createDiary("diary1", diaryDate1, "주절주절", "맑음", travel);
         Diary diary2 = createDiary("diary2", diaryDate2, "절주절주", "흐림", travel);
         Diary diary3 = createDiary("diary3", diaryDate3, "쥬절쥬절", "비", travel);
-        em.persist(diary1);
-        em.persist(diary2);
-        em.persist(diary3);
+        diaryRepository.save(diary1);
+        diaryRepository.save(diary2);
+        diaryRepository.save(diary3);
 
         // When
         em.flush();
         em.clear();
 
-        List<Diary> diaries = diaryRepository.findDiariesByTravel(travel.getId());
+        List<FindDiaryDtoByTravel> diaryDtoByTravels = diaryRepository.findDiaryDtoByTravel(travel.getId());
 
         // Then
-        assertThat(diaries.get(0).getTitle()).isEqualTo("diary1");
-        assertThat(diaries.get(0).getTravelDate()).isEqualTo(diaryDate1);
-        assertThat(diaries.get(0).getWeather()).isEqualTo("맑음");
-        assertThat(diaries.get(0).getMainText()).isEqualTo("주절주절");
+        assertThat(diaryDtoByTravels.get(0).getDiaryId()).isEqualTo(diary1.getId());
+        assertThat(diaryDtoByTravels.get(0).getTitle()).isEqualTo("diary1");
+        assertThat(diaryDtoByTravels.get(0).getTravelDate()).isEqualTo(diaryDate1);
+        assertThat(diaryDtoByTravels.get(0).getWeather()).isEqualTo("맑음");
 
-        assertThat(diaries.get(1).getTitle()).isEqualTo("diary2");
-        assertThat(diaries.get(1).getTravelDate()).isEqualTo(diaryDate2);
-        assertThat(diaries.get(1).getWeather()).isEqualTo("흐림");
-        assertThat(diaries.get(1).getMainText()).isEqualTo("절주절주");
+        assertThat(diaryDtoByTravels.get(1).getDiaryId()).isEqualTo(diary2.getId());
+        assertThat(diaryDtoByTravels.get(1).getTitle()).isEqualTo("diary2");
+        assertThat(diaryDtoByTravels.get(1).getTravelDate()).isEqualTo(diaryDate2);
+        assertThat(diaryDtoByTravels.get(1).getWeather()).isEqualTo("흐림");
 
-        assertThat(diaries.get(2).getTitle()).isEqualTo("diary3");
-        assertThat(diaries.get(2).getTravelDate()).isEqualTo(diaryDate3);
-        assertThat(diaries.get(2).getWeather()).isEqualTo("비");
-        assertThat(diaries.get(2).getMainText()).isEqualTo("쥬절쥬절");
+        assertThat(diaryDtoByTravels.get(2).getDiaryId()).isEqualTo(diary3.getId());
+        assertThat(diaryDtoByTravels.get(2).getTitle()).isEqualTo("diary3");
+        assertThat(diaryDtoByTravels.get(2).getTravelDate()).isEqualTo(diaryDate3);
+        assertThat(diaryDtoByTravels.get(2).getWeather()).isEqualTo("비");
     }
 
     private Member createMember(String id, String name, String pImg, String pIntroduction, LocalDate birthDate, String email, Address address) {
@@ -79,12 +122,55 @@ public class DiaryRepositoryTest {
     }
 
     private Travel createTravel(Member member, String travelDestination, String travelLatitude, String travelLongitude, String travelImage, LocalDate travelStartDate, LocalDate travelEndDate) {
-        LocalDateTime now = LocalDateTime.now();
         return new Travel(now, now, member, travelDestination, travelLatitude, travelLongitude, travelImage, travelStartDate, travelEndDate);
     }
 
     private Diary createDiary(String title, LocalDate travelDate, String mainText, String weather, Travel travel) {
-        LocalDateTime now = LocalDateTime.now();
         return new Diary(now, now, title, travelDate, mainText, weather, travel);
     }
+    
+    /*@Test
+    public void 여행별_일기조회_테스트() {
+
+        // Given
+        Member member = createMember("member1", "Member1", null, null, LocalDate.of(1999, 1, 1), "test@namer.com", null);
+        em.persist(member);
+
+        LocalDate sd = LocalDate.of(2022, 1, 1);
+        LocalDate ed = LocalDate.of(2022, 1, 3);
+        Travel travel = createTravel(member, "서울", "37.541°", "126.986", null, sd, ed);
+        em.persist(travel);
+
+        LocalDate diaryDate1 = LocalDate.of(2022, 1, 3);
+        LocalDate diaryDate2 = LocalDate.of(2022, 1, 2);
+        LocalDate diaryDate3 = LocalDate.of(2022, 1, 1);
+        Diary diary1 = createDiary("diary1", diaryDate1, "주절주절", "맑음", travel);
+        Diary diary2 = createDiary("diary2", diaryDate2, "절주절주", "흐림", travel);
+        Diary diary3 = createDiary("diary3", diaryDate3, "쥬절쥬절", "비", travel);
+        diaryRepository.save(diary1);
+        diaryRepository.save(diary2);
+        diaryRepository.save(diary3);
+
+        // When
+        em.flush();
+        em.clear();
+
+        List<Diary> diaries = diaryRepository.findDiariesByTravel(travel.getId());
+
+        // Then
+        assertThat(diaries.get(2).getTitle()).isEqualTo("diary1");
+        assertThat(diaries.get(2).getTravelDate()).isEqualTo(diaryDate1);
+        assertThat(diaries.get(2).getWeather()).isEqualTo("맑음");
+        assertThat(diaries.get(2).getMainText()).isEqualTo("주절주절");
+
+        assertThat(diaries.get(1).getTitle()).isEqualTo("diary2");
+        assertThat(diaries.get(1).getTravelDate()).isEqualTo(diaryDate2);
+        assertThat(diaries.get(1).getWeather()).isEqualTo("흐림");
+        assertThat(diaries.get(1).getMainText()).isEqualTo("절주절주");
+
+        assertThat(diaries.get(0).getTitle()).isEqualTo("diary3");
+        assertThat(diaries.get(0).getTravelDate()).isEqualTo(diaryDate3);
+        assertThat(diaries.get(0).getWeather()).isEqualTo("비");
+        assertThat(diaries.get(0).getMainText()).isEqualTo("쥬절쥬절");
+    }*/
 }
