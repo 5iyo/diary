eb607fc62c2952a5f990cae7a43158d6ae497fb9
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-11-10 00:10:58 +0900

    TravelService 생성

--- my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/service/TravelService.java
+++ my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/service/TravelService.java
@@ -1,7 +1,29 @@
 package kr.ac.kumoh.oiyo.mydiaryback.service;
 
+import kr.ac.kumoh.oiyo.mydiaryback.domain.Travel;
+import kr.ac.kumoh.oiyo.mydiaryback.repository.TravelRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class TravelService {
+
+    private final TravelRepository travelRepository;
+
+    @Transactional
+    public Long saveTravel(Travel travel) {
+        travelRepository.save(travel);
+        return travel.getId();
+    }
+
+    public Travel findOne(Long travelId) {
+        return travelRepository.findOne(travelId);
+    }
+
+    @Transactional
+    public void deleteTravel(Long travelId) {
+        travelRepository.delete(travelId);
+    }
 }
