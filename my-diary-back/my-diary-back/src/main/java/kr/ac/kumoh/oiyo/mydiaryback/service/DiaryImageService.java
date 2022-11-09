15843df792d79590e04afaa86fd016876b0629ba
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-11-10 00:14:01 +0900

    DiaryImageService 생성

---
+++ my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/service/DiaryImageService.java
@@ -0,0 +1,19 @@
+package kr.ac.kumoh.oiyo.mydiaryback.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class DiaryImageService {
+
+    @Transactional
+    public void save() {
+
+    }
+
+    // 조회 메서드
+
+    // 삭제 메서드
+}
