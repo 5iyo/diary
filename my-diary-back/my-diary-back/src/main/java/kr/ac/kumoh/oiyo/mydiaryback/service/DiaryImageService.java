b8c1fe6e48e5dcdfe487cc1dd703175861220c5d
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-11-11 23:53:58 +0900

    DiaryImageRepository, Servicce 수정

--- my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/service/DiaryImageService.java
+++ my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/service/DiaryImageService.java
@@ -1,19 +1,32 @@
 package kr.ac.kumoh.oiyo.mydiaryback.service;
 
+import kr.ac.kumoh.oiyo.mydiaryback.domain.DiaryImage;
+import kr.ac.kumoh.oiyo.mydiaryback.repository.DiaryImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class DiaryImageService {
 
-    @Transactional
-    public void save() {
+    private final DiaryImageRepository diaryImageRepository;
 
+    @Transactional
+    public Long saveDiaryImage(DiaryImage diaryImage) {
+        return diaryImageRepository.save(diaryImage);
     }
 
     // 조회 메서드
+    public List<DiaryImage> inquiryImagesByDiary(Long diaryId) {
+        return diaryImageRepository.findImagesByDiary(diaryId);
+    }
 
     // 삭제 메서드
+    @Transactional
+    public void deleteDiaryImage(Long diaryImageId) {
+        diaryImageRepository.delete(diaryImageId);
+    }
 }
