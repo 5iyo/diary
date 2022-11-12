bc093e924bbfc7b6a5302f04233817fa2c5ea6e6
Jeonwoojae <dnwo0719@icloud.com>
2022-11-12 12:41:54 +0900

    (server) debug : 카카오에 요청할 때 헤더에 띄워쓰기 고치기

--- my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/controller/KakaoService.java
+++ my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/controller/KakaoService.java
@@ -6,6 +6,9 @@ import com.google.firebase.auth.UserRecord;
 import com.google.firebase.auth.UserRecord.CreateRequest;
 import com.google.firebase.auth.UserRecord.UpdateRequest;
 
+import kr.ac.kumoh.oiyo.mydiaryback.domain.User;
+import kr.ac.kumoh.oiyo.mydiaryback.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
@@ -15,6 +18,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.http.HttpHeaders;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.LogManager;
@@ -29,38 +33,41 @@ import com.google.gson.JsonParser;
 public class KakaoService {
     private static final Logger log = LogManager.getLogManager().getLogger(String.valueOf(KakaoService.class));
 
+    @Autowired
+    private UserRepository mr;
+
 //    getAccessToken()과 getUserInfo()를 호출한다.
-    public Map<String, Object> execKakaoLogin(String authorize_code){
-        Map<String,Object> result = new HashMap<String, Object>();
-        //get access token
-        String accessToken = getAccessKakaoToken(authorize_code);
-        result.put("accessToken",accessToken);
-        //read user info
-        Map<String, Object> userInfo = getKakaoUserInfo(accessToken);
-        result.put("userInfo",userInfo);
-
-//        Firebase Customtoken 발행
-        if(userInfo != null){
-            try{
-                result.put("customToken", createFirebaseCustomToken(userInfo));
-                result.put("errYn","N");
-                result.put("errMsg","");
-            }catch (FirebaseAuthException e){
-                result.put("errYn", "Y");
-                result.put("errMsg","FirebaseException : "+e.getMessage());
-            }catch (Exception e){
-                result.put("errYn","Y");
-                result.put("errMsg","Kakap Login Fail");
-            }
-
-            log.info(userInfo.toString());
-            return result;
-        }
-
-
-        System.out.println(userInfo.toString());
-        return userInfo;
-    }
+//    public Map<String, Object> execKakaoLogin(String authorize_code){
+//        Map<String,Object> result = new HashMap<String, Object>();
+//        //get access token
+//        String accessToken = getAccessKakaoToken(authorize_code);
+//        result.put("accessToken",accessToken);
+//        //read user info
+//        Map<String, Object> userInfo = getKakaoUserInfo(accessToken);
+//        result.put("userInfo",userInfo);
+//
+////        Firebase Customtoken 발행
+//        if(userInfo != null){
+//            try{
+//                result.put("customToken", createFirebaseCustomToken(userInfo));
+//                result.put("errYn","N");
+//                result.put("errMsg","");
+//            }catch (FirebaseAuthException e){
+//                result.put("errYn", "Y");
+//                result.put("errMsg","FirebaseException : "+e.getMessage());
+//            }catch (Exception e){
+//                result.put("errYn","Y");
+//                result.put("errMsg","Kakap Login Fail");
+//            }
+//
+//            log.info(userInfo.toString());
+//            return result;
+//        }
+//
+//
+//        System.out.println(userInfo.toString());
+//        return userInfo;
+//    }
 
 //    인가 코드를 받아서 카카오에 AccessToken을 요청하고,
 //    전달 받은 AccessToken을 return한다.
@@ -81,15 +88,15 @@ public class KakaoService {
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
             StringBuilder sb = new StringBuilder();
             sb.append("grant_type=authorization_code");
-            sb.append("&client_id=YOUR_API_KEY");
-            sb.append("&redirect_uri=http://YOUR_IP_ADRESS:8080/kakao/login");
+            sb.append("&client_id=22dc6a1f1f08667d709fdbb48d7d6ccf");
+            sb.append("&redirect_uri=http://localhost:8080/kakao/login");
             sb.append("&code="+authorize_code);
             bw.write(sb.toString());
             bw.flush();
 
             //결과 코드가 200이면 성공
             int responseCode = conn.getResponseCode();
-            log.info("responseCode : "+responseCode);
+            System.out.println("responseCode : "+responseCode);
 
             //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
             BufferedReader br = new BufferedReader((new InputStreamReader(conn.getInputStream())));
@@ -99,7 +106,7 @@ public class KakaoService {
             while((line = br.readLine()) != null){
                 result += line;
             }
-            log.info("responsebody : "+result);
+            System.out.println("responsebody : "+result);
 
             //Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
             JsonElement element = JsonParser.parseString(result);
@@ -107,8 +114,8 @@ public class KakaoService {
             access_Token = element.getAsJsonObject().get("access_token").getAsString();
             refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
 
-            log.info("access_token : " + access_Token);
-            log.info("refresh_token : " + refresh_Token);
+            System.out.println("access_token : " + access_Token);
+            System.out.println("refresh_token : " + refresh_Token);
 
             br.close();
             bw.close();
@@ -129,10 +136,10 @@ public class KakaoService {
             conn.setRequestMethod("GET");
 
             //요청에 필요한 Header에 포함될 내용
-            conn.setRequestProperty("Authorization", "Bearer" + access_Token);
+            conn.setRequestProperty("Authorization", "Bearer " + access_Token);
 
             int responseCode = conn.getResponseCode();
-            log.info("responseCode : " + responseCode);
+            System.out.println("responseCode : " + responseCode);
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
             String line = "";
@@ -141,7 +148,7 @@ public class KakaoService {
             while ((line = br.readLine()) != null) {
                 result += line;
             }
-            log.info("response body : " + result);
+            System.out.println("response body : " + result);
 
             JsonElement element = JsonParser.parseString(result);
             JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
@@ -172,11 +179,11 @@ public class KakaoService {
             conn.setDoOutput(true);
 
 //            요청에 필요한 Header에 포함될 내용
-             conn.setRequestProperty("Authorization", "Bearer"+access_Token);
+             conn.setRequestProperty("Authorization", "Bearer "+access_Token);
 
 //             결과 코드가 200이라면 성공
             int responseCode = conn.getResponseCode();
-            log.info("responseCode : "+responseCode);
+            System.out.println("responseCode : "+responseCode);
 
 //            요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
@@ -186,7 +193,7 @@ public class KakaoService {
             while ((line = br.readLine()) != null){
                 result +=line;
             }
-            log.info("response body : "+result);
+            System.out.println("response body : "+result);
             br.close();
         }catch (IOException e){
             e.printStackTrace();
@@ -194,28 +201,28 @@ public class KakaoService {
         return access_Token;
     }
 
-    public String createFirebaseCustomToken(Map<String, Object> userInfo) throws Exception{
-        UserRecord userRecord;
-        String uid = userInfo.get("id").toString();
-        String email = userInfo.get("email").toString();
-        String displayName =  userInfo.get("nickname").toString();
-
-//        1. 사용자 정보로 파이어 베이스 유저정보 update, 사용자 정보가 있다면 userRecord에 유저 정보가 담긴다.
-        try {
-            UpdateRequest request = new UpdateRequest(uid);
-            request.setEmail(email);
-            request.setDisplayName(displayName);
-            userRecord = FirebaseAuth.getInstance().updateUser(request);
-//      1-2. 사용자 정보가 없다면 catch에서 createUser로 사용자를 생성한다.
-        }catch (FirebaseAuthException e){
-            CreateRequest createRequest = new CreateRequest();
-            createRequest.setUid(uid);
-            createRequest.setEmail(email);
-            createRequest.setEmailVerified(false);
-            createRequest.setDisplayName(displayName);
-
-            userRecord = FirebaseAuth.getInstance().createUser(createRequest);
-        }
-        return FirebaseAuth.getInstance().createCustomToken(userRecord.getUid());
-    }
+//    public String createFirebaseCustomToken(Map<String, Object> userInfo) throws Exception{
+//        UserRecord userRecord;
+//        String uid = userInfo.get("id").toString();
+//        String email = userInfo.get("email").toString();
+//        String displayName =  userInfo.get("nickname").toString();
+//
+////        1. 사용자 정보로 파이어 베이스 유저정보 update, 사용자 정보가 있다면 userRecord에 유저 정보가 담긴다.
+//        try {
+//            UpdateRequest request = new UpdateRequest(uid);
+//            request.setEmail(email);
+//            request.setDisplayName(displayName);
+//            userRecord = FirebaseAuth.getInstance().updateUser(request);
+////      1-2. 사용자 정보가 없다면 catch에서 createUser로 사용자를 생성한다.
+//        }catch (FirebaseAuthException e){
+//            CreateRequest createRequest = new CreateRequest();
+//            createRequest.setUid(uid);
+//            createRequest.setEmail(email);
+//            createRequest.setEmailVerified(false);
+//            createRequest.setDisplayName(displayName);
+//
+//            userRecord = FirebaseAuth.getInstance().createUser(createRequest);
+//        }
+//        return FirebaseAuth.getInstance().createCustomToken(userRecord.getUid());
+//    }
 }
