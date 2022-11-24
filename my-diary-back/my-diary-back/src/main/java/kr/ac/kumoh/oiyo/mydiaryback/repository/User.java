f508fbb0a8021faf244a91db785659dbb811eaab
Jeonwoojae <dnwo0719@icloud.com>
2022-11-24 15:42:31 +0900

    (server) feat:User의 추가정보만 getter,setter

--- my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/repository/User.java
+++ my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/repository/User.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 @NoArgsConstructor
@@ -27,12 +29,38 @@ public class User extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private UserRole role;
 
+    @Column
+    private String profileIntroduction;
+
+    @Column
+    private LocalDate birthDate;
+
+    @Column
+    private String address;
+
     @Builder
     public User(String username, String email, UserRole role, String profileImage){
         this.username = username;
         this.email = email;
         this.profileImage = profileImage;
         this.role = role;
+    }
+
+    public void setProfileIntroduction(String profileIntroduction){
+        this.profileIntroduction = profileIntroduction;
+    }
+    public void setBirthDate(LocalDate birthDate)
+    {
+        this.birthDate = birthDate;
+    }
+
+    public void setBirthDatebyString(String birthDate){
+        LocalDate date = LocalDate.parse(birthDate, DateTimeFormatter.ISO_DATE);
+        this.birthDate = date;
+    }
+
+    public void setAddress(String address){
+        this.address = address;
     }
 
 
