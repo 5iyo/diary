7703f0d97c2005118205eec326471ac795097877
Jeonwoojae <dnwo0719@icloud.com>
2022-11-24 15:40:57 +0900

    (server) fix:PostUserInfoDto의 데이터는 모두 String

--- my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/repository/PostUserInfoDto.java
+++ my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/repository/PostUserInfoDto.java
@@ -2,12 +2,10 @@ package kr.ac.kumoh.oiyo.mydiaryback.repository;
 
 import lombok.Data;
 
-import java.time.LocalDate;
-
 @Data
 public class PostUserInfoDto {
     private String email;
     private String profileIntroduction;
-    private LocalDate birthDate;
+    private String birthDate;
     private String address;
 }
