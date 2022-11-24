3d61854c675989e1ce7897a6a9b725724c6937a4
Jeonwoojae <dnwo0719@icloud.com>
2022-11-24 15:43:57 +0900

    (server) feat:Controller에 User 추가 정보 POST로 수정

--- my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/controller/UserController.java
+++ my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/controller/UserController.java
@@ -1,7 +1,8 @@
 package kr.ac.kumoh.oiyo.mydiaryback.controller;
 
+import kr.ac.kumoh.oiyo.mydiaryback.repository.PostUserInfoDto;
 import kr.ac.kumoh.oiyo.mydiaryback.repository.User;
-import kr.ac.kumoh.oiyo.mydiaryback.repository.UserRepository;
+import kr.ac.kumoh.oiyo.mydiaryback.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -10,10 +11,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RequiredArgsConstructor
-@RequestMapping(value = "/users", produces = {MediaType.APPLICATION_JSON_VALUE})
+@RequestMapping(value = "/user", produces = {MediaType.APPLICATION_JSON_VALUE})
 @RestController
 public class UserController {
-    private final UserRepository userRepository;
+    private final UserService userService;
+
+    @PostMapping("")
+    public Map<String, Object> updateUser(@RequestBody PostUserInfoDto updateDto){
+        Map<String, Object>response = new HashMap<>();
+         if(userService.updateUser(updateDto) == 1)
+         {
+             response.put("result","SUCCESS");
+         }else{
+             response.put("result","FAIL");
+             response.put("reason","사용자 정보 수정 실패");
+         }
+        return response;
+    }
 
     @GetMapping("/{id}")
     public Map<String, Object> findById(@PathVariable("id")User user){
