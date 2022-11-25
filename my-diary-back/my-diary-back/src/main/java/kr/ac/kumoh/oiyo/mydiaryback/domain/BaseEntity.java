8122f5240a0c3f29ed42b46fb65ef0bc0293de9f
Jeonwoojae <dnwo0719@icloud.com>
2022-11-25 10:25:34 +0900

    (server) fix:baseEntity 자동으로 값 매핑 수정

--- my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/domain/BaseEntity.java
+++ my-diary-back/my-diary-back/src/main/java/kr/ac/kumoh/oiyo/mydiaryback/domain/BaseEntity.java
@@ -1,21 +1,18 @@
 package kr.ac.kumoh.oiyo.mydiaryback.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     // 생성일자
