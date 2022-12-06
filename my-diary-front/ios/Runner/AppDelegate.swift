55e35d8570465d333c812a18c867893e4c1eb13e
parkjunsik <akfldkskgorn@gmail.com>
2022-12-06 15:32:52 +0900

    fix: Google maps flutter ios 지원

--- my-diary-front/ios/Runner/AppDelegate.swift
+++ my-diary-front/ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import NaverThirdPartyLogin
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +9,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBOZ04Hq7J0MnjUhQP2Mw6e264cBM9SjL4")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
