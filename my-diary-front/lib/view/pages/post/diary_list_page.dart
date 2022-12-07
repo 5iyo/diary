40066f2a648dbd089df493d1468b22548d2f3b0e
parkjunsik <akfldkskgorn@gmail.com>
2022-12-07 19:32:06 +0900

    refactor: DiaryListPage UiViewModel 적용

--- my-diary-front/lib/view/pages/post/diary_list_page.dart
+++ my-diary-front/lib/view/pages/post/diary_list_page.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:my_diary_front/controller/provider/diarylist_provider.dart';
+import 'package:my_diary_front/view/components/ui_view_model.dart';
 import 'package:my_diary_front/view/pages/post/diary_page.dart';
 import 'package:my_diary_front/view/pages/post/write_page.dart';
 
 import 'package:provider/provider.dart';
 
 class DiaryListPage extends StatefulWidget {
-
   final int? id;
+
   const DiaryListPage(this.id);
 
   @override
@@ -28,6 +29,7 @@ class _DiaryListPage extends State<DiaryListPage> {
 
   var refreshKey = GlobalKey<RefreshIndicatorState>();
   final int id;
+
   _DiaryListPage(this.id);
 
   @override
@@ -37,49 +39,45 @@ class _DiaryListPage extends State<DiaryListPage> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset : false,
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         systemOverlayStyle: SystemUiOverlayStyle.dark,
-        title: Text("Diary List",
-            style: TextStyle(color: Colors.grey[700])),
+        title: Text("Diary List", style: TextStyle(color: Colors.grey[700])),
         centerTitle: true,
         backgroundColor: Colors.transparent,
         elevation: 0.0,
       ),
       extendBodyBehindAppBar: true,
-      body: Container(
-        decoration: BoxDecoration(
-            color: Colors.white,
-            image: DecorationImage(
-                image: Image.asset("img/login_background.png").image,
-                fit: BoxFit.fitHeight)),
-        child: Consumer<DiaryListProvider>(
-          builder: (context, DiaryListProvider value, child) {
-            if(value.diaries != null && value.diaries.length>0) {
-              return Padding(
-                  padding: EdgeInsets.only(top: 10),
-                  child: Center(
-                    child: ListView.separated(
-                      itemCount: value.diaries.length ?? 0,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          onTap: (){
-                            Get.to(()=>DiaryPage(value.diaries[index].id, id));
-                          },
-                          title: Text("${value.diaries[index].title}"),
-                        );
-                      },
-                      separatorBuilder: (context, index) {
-                        return Divider();
-                      },
-                    ),
-                  )
-              );
-            }
-            return Center(child: Text('일기를 추가해주세요'));
-          },
-        ),
-      ),
+      body: UiViewModel.buildBackgroundContainer(
+          context: context,
+          backgroundType: BackgroundType.write,
+          child:
+              UiViewModel.buildSizedLayout(context, Consumer<DiaryListProvider>(
+            builder: (context, DiaryListProvider value, child) {
+              if (value.diaries != null && value.diaries.length > 0) {
+                return Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: Center(
+                      child: ListView.separated(
+                        itemCount: value.diaries.length ?? 0,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            onTap: () {
+                              Get.to(
+                                  () => DiaryPage(value.diaries[index].id, id));
+                            },
+                            title: Text("${value.diaries[index].title}"),
+                          );
+                        },
+                        separatorBuilder: (context, index) {
+                          return Divider();
+                        },
+                      ),
+                    ));
+              }
+              return Center(child: Text('일기를 추가해주세요'));
+            },
+          ))),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Get.off(() => WritePage(id));
