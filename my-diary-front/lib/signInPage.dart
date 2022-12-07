79308aa42de9ab04f09f46e212d94d7d75d863e7
parkjunsik <akfldkskgorn@gmail.com>
2022-12-08 03:37:08 +0900

    style: signInPage progress bar 추가

--- my-diary-front/lib/signInPage.dart
+++ my-diary-front/lib/signInPage.dart
@@ -22,6 +22,8 @@ class _SignInPageState extends State<SignInPage> {
 
   late MainViewModel _mainViewModel;
 
+  bool isAwait = false;
+
   @override
   void initState() {
     super.initState();
@@ -32,65 +34,99 @@ class _SignInPageState extends State<SignInPage> {
     _mainViewModel = Provider.of<MainViewModel>(context, listen: true);
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: UiViewModel.buildBackgroundContainer(
-            context: context,
-            backgroundType: BackgroundType.login,
-            child: Column(
-              children: [
-                const Spacer(flex: 17,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Spacer(
-                      flex: 1,
-                    ),
-                    ElevatedButton(
-                      onPressed: () => _mainViewModel.login(NaverLogin()),
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.transparent, elevation: 0.0),
-                      child: Image.asset(
-                        'img/naver_icon.png',
-                        height: 50,
-                        fit: BoxFit.fitHeight,
+        body: Stack(children: [
+          SingleChildScrollView(
+            child: UiViewModel.buildBackgroundContainer(
+              context: context,
+              backgroundType: BackgroundType.login,
+              child: Column(
+                children: [
+                  const Spacer(
+                    flex: 17,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Spacer(
+                        flex: 1,
                       ),
-                    ),
-                    const Spacer(
-                      flex: 2,
-                    ),
-                    ElevatedButton(
-                      onPressed: () => _mainViewModel.login(KakaoLogin()),
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.transparent, elevation: 0.0),
-                      child: Image.asset(
-                        'img/kakao_icon.png',
-                        height: 50,
-                        fit: BoxFit.fitHeight,
+                      ElevatedButton(
+                        onPressed: () async {
+                          setState(() {
+                            isAwait = true;
+                          });
+                          await _mainViewModel.login(NaverLogin());
+                          setState(() {
+                            isAwait = false;
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.transparent,
+                            elevation: 0.0),
+                        child: Image.asset(
+                          'img/naver_icon.png',
+                          height: 50,
+                          fit: BoxFit.fitHeight,
+                        ),
                       ),
-                    ),
-                    const Spacer(
-                      flex: 2,
-                    ),
-                    ElevatedButton(
-                      onPressed: () => _mainViewModel.login(GoogleLogin()),
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.transparent, elevation: 0.0),
-                      child: Image.asset(
-                        'img/google_icon.png',
-                        height: 50,
-                        fit: BoxFit.fitHeight,
+                      const Spacer(
+                        flex: 2,
                       ),
-                    ),
-                    const Spacer(
-                      flex: 1,
-                    ),
-                  ],
-                ),
-                const Spacer(flex: 3,),
-              ],
+                      ElevatedButton(
+                        onPressed: () async {
+                          setState(() {
+                            isAwait = true;
+                          });
+                          await _mainViewModel.login(KakaoLogin());
+                          setState(() {
+                            isAwait = false;
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.transparent,
+                            elevation: 0.0),
+                        child: Image.asset(
+                          'img/kakao_icon.png',
+                          height: 50,
+                          fit: BoxFit.fitHeight,
+                        ),
+                      ),
+                      const Spacer(
+                        flex: 2,
+                      ),
+                      ElevatedButton(
+                        onPressed: () async {
+                          setState(() {
+                            isAwait = true;
+                          });
+                          await _mainViewModel.login(GoogleLogin());
+                          setState(() {
+                            isAwait = false;
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.transparent,
+                            elevation: 0.0),
+                        child: Image.asset(
+                          'img/google_icon.png',
+                          height: 50,
+                          fit: BoxFit.fitHeight,
+                        ),
+                      ),
+                      const Spacer(
+                        flex: 1,
+                      ),
+                    ],
+                  ),
+                  const Spacer(
+                    flex: 3,
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
+          isAwait ? UiViewModel.buildProgressBar() : Container(),
+        ]),
       ),
     );
   }
